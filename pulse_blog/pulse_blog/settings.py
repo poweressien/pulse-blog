2acaa584dd1b6528c7092f5077119aa7bb7986c5
poweressien <essiengodspower447@gmail.com>
2026-06-14 16:49:42 +0100

    Add Render deployment config

--- pulse_blog/pulse_blog/settings.py
+++ pulse_blog/pulse_blog/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,10 +52,10 @@ TEMPLATES = [{
 WSGI_APPLICATION = 'pulse_blog.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
