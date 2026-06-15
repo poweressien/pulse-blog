4e229e30cec3e8ce5cdcf0f665211b1f2cd81669
iNWEKOBONG <essiengodspower447@gmail.com>
2026-06-15 21:50:24 +0100

    Update settings.py

--- pulse_blog/pulse_blog/settings.py
+++ pulse_blog/pulse_blog/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from decouple import config
 import dj_database_url
+from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
